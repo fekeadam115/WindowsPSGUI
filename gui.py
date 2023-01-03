9f9382ec3851d9cd624ae0105c290a3b09ca8e00
Adam Feke <fekeadam115@gmail.com>
2023-01-02 19:48:34 -0800

    Console Log
    Added console log so that command outputs in a text box

--- gui.py
+++ gui.py
@@ -1,3 +1,7 @@
+# Windows PowerShell GUI
+# Author: Adam Feke
+# Created: 1/1/2023
+import tkinter
 from tkinter import *
 import subprocess
 
@@ -5,22 +9,53 @@ root = Tk()
 root.title("PowerShell GUI")
 root.iconbitmap("images/powershell.ico")
 
+f2 = Frame(root)
+f2.grid(row=0, column=2)
 
-def buttonCommand(command, row, col):
+text_consolecommand = Label(f2, text="", justify=LEFT, fg="green")
+text_consolecommand.grid(row=1, column=1)
+
+
+def buttonCommand(command):
     output = subprocess.run(["powershell", "-Command", command], capture_output=True)
     result = output.stdout.decode("utf-8")
     result = result.strip()
-    label_datetime = Label(root, text=result)
-    label_datetime.grid(row=row, column=col, padx=20, pady=20)
+    header_text = "Ran: " + command
+    text_consolecommand = Label(f2, text=header_text, justify=LEFT, fg="green")
+    text_consolecommand.grid(row=1, column=1)
+    text.insert(END, result + '\n\n')
 
 
-button_datetime = Button(root, text="Date and Time", command=lambda: buttonCommand("Get-Date", 0, 1))
-button_datetime.grid(row=0, column=0, padx=20, pady=20)
+def clearConsoleLog():
+    text.delete("1.0", END)
 
-button_powercfg = Button(root, text="List Power Plans", command=lambda: buttonCommand("powercfg /list", 1, 1))
-button_powercfg.grid(row=1, column=0, padx=20, pady=20)
 
-#label_datetime = Label(root, text=" ")
-#label_datetime.grid(row=0, column=1, padx=20, pady=20)
+
+# Console Log text above the text box
+label_consolelog = Label(f2, text="Console Log")
+label_consolelog.grid(row=0, column=1, padx=20, pady=20)
+
+# Console Log
+text = Text(root, height=25, width=75, bd=3, padx=20, pady=20)
+text.grid(row=1, column=2, columnspan=3)
+
+# Clear button
+button_clear = Button(f2, text="Clear", command=clearConsoleLog, width=10)
+button_clear.grid(row=0, column=2, padx=20, pady=20)
+
+
+# Buttons on the left side
+# Frame
+f1 = Frame(root)
+f1.grid(row=1, column=0, rowspan=2)
+
+# Date and Time button
+button_datetime = Button(f1, text="Date and Time", command=lambda: buttonCommand("Get-Date"))
+button_datetime.grid(row=1, column=0, padx=20, pady=20)
+
+# List Power Plan Button
+button_powercfg = Button(f1, text="List Power Plans", command=lambda: buttonCommand("powercfg /list"))
+button_powercfg.grid(row=2, column=0, padx=20, pady=20)
+
 
 root.mainloop()
